2ad6c25ecc64a7c25e900b5c3c8e45bd68379828
zhangb14 <Zb405133134>
2020-07-17 16:28:37 +0800

    init

--- src/api/control/index.js
+++ src/api/control/index.js
@@ -1,19 +1,28 @@
 import request from '@/utils/request'
 
-export function controlDevice(listTime) {
+export function controlDevice(listTime, deviceNum) {
   return request({
     url: '/device/control',
     method: 'post',
     data: {
-      startTemp: listTime.startTemp,
-      startTime: listTime.startTime,
-      upSpeed: listTime.upSpeed,
-      constantTemp: listTime.constantTemp,
-      constantTime: listTime.constantTime,
-      downSpeed: listTime.downSpeed,
-      endTemp: listTime.endTemp,
-      taskNum: listTime.deviceNum[0],
-      deviceNum: listTime.deviceNum[1]
+      taskNum: deviceNum.deviceNum[0],
+      deviceNum: deviceNum.deviceNum[1],
+      modelType: listTime.modelType,
+      tapControl1: listTime.tapControl1,
+      tapControl2: listTime.tapControl2,
+      probeType1: listTime.probeType1,
+      probeType2: listTime.probeType2,
+      tempControl: listTime.tempControl
+    }
+  })
+}
+
+export function getDeviceControlInfo(deviceNum) {
+  return request({
+    url: '/device/control',
+    method: 'post',
+    data: {
+      deviceNum: deviceNum
     }
   })
 }
--- src/views/control/index.vue
+++ src/views/control/index.vue
@@ -1,24 +1,26 @@
 <template>
   <div class="dashboard-editor-container">
-    <el-form ref="listTime" :model="listTime" :rules="rules" label-width="auto">
-      <div class="panel-group" style="background-color:white">
-        <el-cascader v-model="listTime.deviceNum" :options="options" style="width: 300px" placeholder="请选择" filterable clearable />
-      </div>
-      <div class="panel-group" style="background-color:white">
-        <el-steps :active="active" finish-status="success">
-          <el-step title="温度模式选择" />
-          <el-step title="阀门控制" />
-          <el-step title="选择探头" />
-          <el-step title="设置温度" />
-        </el-steps>
-      </div>
+    <div class="panel-group" style="background-color:white">
+      <el-cascader v-model="deviceNum" :options="options" style="width: 300px" placeholder="请选择" filterable clearable />
+    </div>
+    <div class="panel-group" style="background-color:white">
+      <el-steps :active="active" finish-status="success">
+        <el-step title="温度模式选择" />
+        <el-step title="阀门控制" />
+        <el-step title="选择探头" />
+        <el-step title="设置温度" />
+      </el-steps>
+    </div>
+    <el-form ref="listTime" :model="listTime" label-width="auto">
       <div class="panel-group" style="background-color:white">
         <el-tabs v-model="active" tab-position="left" @tab-click="handleClick">
           <el-tab-pane label="温度模式选择" name="0">
             <div style="text-align:center">
               <el-form-item>
-                <el-radio v-model="listTime.modelType" label="00">采集器模式</el-radio>
-                <el-radio v-model="listTime.modelType" label="01">阀门控制器模式</el-radio>
+                <el-radio-group v-model="listTime.modelType">
+                  <el-radio label="00">采集器模式</el-radio>
+                  <el-radio label="01">阀门控制器模式</el-radio>
+                </el-radio-group>
               </el-form-item>
             </div>
           </el-tab-pane>
@@ -26,16 +28,30 @@
             <el-row :gutter="20">
               <el-col :span="12">
                 <el-form-item label="阀门1:">
-                  <el-radio v-model="listTime.modelType" label="00">关闭</el-radio>
-                  <el-radio v-model="listTime.modelType" label="01">启动</el-radio>
+                  <el-radio-group v-model="listTime.tapControl1">
+                    <el-radio label="00">关闭</el-radio>
+                    <el-radio label="01">启动（或重新启动），等待温度到达</el-radio>
+                    <el-radio label="02">等恒温状态时间到达</el-radio>
+                    <el-radio label="03">升温过程，等待升温时间到</el-radio>
+                    <el-radio label="04">高温恒定状态，等待恒温时间到</el-radio>
+                    <el-radio label="05">降温过程，等待降温时间到</el-radio>
+                    <el-radio label="06">等待降温温度到</el-radio>
+                  </el-radio-group>
                 </el-form-item>
               </el-col>
             </el-row>
             <el-row :gutter="20">
               <el-col :span="12">
                 <el-form-item label="阀门2:">
-                  <el-radio v-model="listTime.modelType" label="00">关闭</el-radio>
-                  <el-radio v-model="listTime.modelType" label="01">启动</el-radio>
+                  <el-radio-group v-model="listTime.tapControl2">
+                    <el-radio label="00">关闭</el-radio>
+                    <el-radio label="01">启动（或重新启动），等待温度到达</el-radio>
+                    <el-radio label="02">等恒温状态时间到达</el-radio>
+                    <el-radio label="03">升温过程，等待升温时间到</el-radio>
+                    <el-radio label="04">高温恒定状态，等待恒温时间到</el-radio>
+                    <el-radio label="05">降温过程，等待降温时间到</el-radio>
+                    <el-radio label="06">等待降温温度到</el-radio>
+                  </el-radio-group>
                 </el-form-item>
               </el-col>
             </el-row>
@@ -44,16 +60,32 @@
             <el-row :gutter="20">
               <el-col :span="12">
                 <el-form-item label="阀门1:">
-                  <el-radio v-model="listTime.modelType" label="00">关闭</el-radio>
-                  <el-radio v-model="listTime.modelType" label="01">启动</el-radio>
+                  <el-checkbox-group v-model="listTime.probeType1">
+                    <el-checkbox label="1">T0</el-checkbox>
+                    <el-checkbox label="2">T1</el-checkbox>
+                    <el-checkbox label="4">T2</el-checkbox>
+                    <el-checkbox label="8">T3</el-checkbox>
+                    <el-checkbox label="16">T4</el-checkbox>
+                    <el-checkbox label="32">T5</el-checkbox>
+                    <el-checkbox label="64">T6</el-checkbox>
+                    <el-checkbox label="128">T7</el-checkbox>
+                  </el-checkbox-group>
                 </el-form-item>
               </el-col>
             </el-row>
             <el-row :gutter="20">
               <el-col :span="12">
                 <el-form-item label="阀门2:">
-                  <el-radio v-model="listTime.modelType" label="00">关闭</el-radio>
-                  <el-radio v-model="listTime.modelType" label="01">启动</el-radio>
+                  <el-checkbox-group v-model="listTime.probeType2">
+                    <el-checkbox label="1">T0</el-checkbox>
+                    <el-checkbox label="2">T1</el-checkbox>
+                    <el-checkbox label="4">T2</el-checkbox>
+                    <el-checkbox label="8">T3</el-checkbox>
+                    <el-checkbox label="16">T4</el-checkbox>
+                    <el-checkbox label="32">T5</el-checkbox>
+                    <el-checkbox label="64">T6</el-checkbox>
+                    <el-checkbox label="128">T7</el-checkbox>
+                  </el-checkbox-group>
                 </el-form-item>
               </el-col>
             </el-row>
@@ -62,58 +94,52 @@
             <el-row :gutter="20">
               <el-col :span="6">
                 <el-form-item label="静停温度(℃):" prop="startTemp">
-                  <el-input v-model="listTime.startTemp" placeholder="静停温度" clearable />
+                  <el-input v-model="listTime.tempControl.startTemp" placeholder="静停温度" clearable />
                 </el-form-item>
               </el-col>
               <el-col :span="6">
                 <el-form-item label="静停时长(h):" prop="startTime">
-                  <el-input v-model="listTime.startTime" placeholder="静停时长" clearable />
+                  <el-input v-model="listTime.tempControl.startTime" placeholder="静停时长" clearable />
                 </el-form-item>
               </el-col>
               <el-col :span="6">
                 <el-form-item label="升温速度(℃/h):" prop="upSpeed">
-                  <el-input v-model="listTime.upSpeed" placeholder="升温速度" clearable />
+                  <el-input v-model="listTime.tempControl.upSpeed" placeholder="升温速度" clearable />
                 </el-form-item>
               </el-col>
               <el-col :span="6">
                 <el-form-item label="恒温温度(℃):" prop="constantTemp">
-                  <el-input v-model="listTime.constantTemp" placeholder="恒温温度" clearable />
+                  <el-input v-model="listTime.tempControl.constantTemp" placeholder="恒温温度" clearable />
                 </el-form-item>
               </el-col>
               <el-col :span="6">
                 <el-form-item label="恒温时长(h):" prop="constantTime">
-                  <el-input v-model="listTime.constantTime" placeholder="恒温时长" clearable />
+                  <el-input v-model="listTime.tempControl.constantTime" placeholder="恒温时长" clearable />
                 </el-form-item>
               </el-col>
               <el-col :span="6">
                 <el-form-item label="降温速度(℃/h):" prop="downSpeed">
-                  <el-input v-model="listTime.downSpeed" placeholder="降温速度" clearable />
+                  <el-input v-model="listTime.tempControl.downSpeed" placeholder="降温速度" clearable />
                 </el-form-item>
               </el-col>
               <el-col :span="6">
                 <el-form-item label="结束温度(℃):" prop="endTemp">
-                  <el-input v-model="listTime.endTemp" placeholder="结束温度" clearable />
-                </el-form-item>
-              </el-col>
-              <el-col :span="12">
-                <el-form-item label="设备编号:" prop="deviceNum">
-                  <el-cascader v-model="listTime.deviceNum" :options="options" style="width: 300px" placeholder="请选择" filterable clearable />
+                  <el-input v-model="listTime.tempControl.endTemp" placeholder="结束温度" clearable />
                 </el-form-item>
               </el-col>
               <el-col :span="12">
                 <el-button style="background-color: #42b983;" type="success" @click="btnQuery()">查看数据图像</el-button>
-                <el-button style="background-color: #42b983;" type="success" @click="btnControl()">设置温度控制阀门参数</el-button>
-                <el-button style="background-color: #42b983;" type="success" @click="btnControl()">温度模式选择</el-button>
-                <el-button style="background-color: #42b983;" type="success" @click="btnControl()">开启或关闭阀门控制</el-button>
-                <el-button style="background-color: #42b983;" type="success" @click="btnControl()">阀门控制选择探头</el-button>
               </el-col>
             </el-row>
+            <div class="panel-group" style="background-color:white">
+              <div id="chartDiv" class="chart-container">
+                <div id="mnsjChart" style="width:100%;height:400px;" />
+              </div>
+            </div>
           </el-tab-pane>
         </el-tabs>
-      </div>
-      <div class="panel-group" style="background-color:white">
-        <div id="chartDiv" class="chart-container">
-          <div id="mnsjChart" style="width:100%;height:400px;" />
+        <div>
+          <el-button style="background-color: #42b983;" type="success" @click="btnControl()">控制下发</el-button>
         </div>
       </div>
     </el-form>
@@ -123,59 +149,29 @@
 <script>
 import echarts from 'echarts'
 import { listTaskDevice } from '@/api/base/index'
-import { controlDevice } from '@/api/control/index'
+import { controlDevice, getDeviceControlInfo } from '@/api/control/index'
 
 export default {
   data() {
-    var validateDouble = (rule, value, callback) => {
-      var pattern = /^\d+\.?\d{0,1}$/
-      if (value === '') {
-        return callback(new Error('请输入参数'))
-      } else if (!pattern.test(value)) {
-        return callback(new Error('小数点后最多只能输入一位'))
-      } else {
-        callback()
-      }
-    }
     return {
       active: 0,
-      rules: {
-        startTemp: [
-          { required: true, trigger: 'blur', validator: validateDouble }
-        ],
-        startTime: [
-          { required: true, trigger: 'blur', validator: validateDouble }
-        ],
-        upSpeed: [
-          { required: true, trigger: 'blur', validator: validateDouble }
-        ],
-        constantTemp: [
-          { required: true, validator: validateDouble, trigger: 'blur' }
-        ],
-        constantTime: [
-          { required: true, validator: validateDouble, trigger: 'blur' }
-        ],
-        downSpeed: [
-          { required: true, validator: validateDouble, trigger: 'blur' }
-        ],
-        endTemp: [
-          { required: true, validator: validateDouble, trigger: 'blur' }
-        ],
-        deviceNum: [
-          { type: 'array', required: true, message: '请输入设备编号', trigger: 'change' }
-        ]
-      },
       chart: null,
+      deviceNum: [],
       listTime: {
-        startTemp: '',
-        startTime: '',
-        upSpeed: '',
-        constantTemp: '',
-        constantTime: '',
-        downSpeed: '',
-        endTemp: '',
-        deviceNum: [],
-        modelType: '00'
+        modelType: '',
+        tapControl1: '',
+        tapControl2: '',
+        probeType1: [0],
+        probeType2: [0],
+        tempControl: {
+          startTemp: '',
+          startTime: '',
+          upSpeed: '',
+          constantTemp: '',
+          constantTime: '',
+          downSpeed: '',
+          endTemp: ''
+        }
 
       },
       options: []
@@ -188,9 +184,14 @@ export default {
     getDeviceNum() {
       listTaskDevice().then(resp => {
         this.options = resp.data
-        this.listTime.deviceNum.push(resp.data[0].value)
-        this.listTime.deviceNum.push(resp.data[0].children[0].value)
+        this.deviceNum.push(resp.data[0].value)
+        this.deviceNum.push(resp.data[0].children[0].value)
         this.initChart()
+      })
+    },
+    getDeviceControlInfo() {
+      getDeviceControlInfo().then(resp => {
+        this.listTime = resp.data
       })
     },
     initChart() {
@@ -198,36 +199,23 @@ export default {
       this.chart = echarts.init(contain)
     },
     btnControl() {
-      this.$refs.listTime.validate(valid => {
-        if (valid) {
-          controlDevice(this.listTime).then(resp => {
-            alert('下发成功')
-          })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
+      controlDevice(this.listTime, this.deviceNum).then(resp => {
+        alert('控制成功')
       })
     },
     btnQuery() {
-      this.$refs.listTime.validate(valid => {
-        if (valid) {
-          var xData = [0, this.listTime.startTime]
-          var upTime = Number((this.listTime.constantTemp - this.listTime.startTemp) / this.listTime.upSpeed) + Number(this.listTime.startTime)
-          xData.push(upTime)
-          var constant = upTime + Number(this.listTime.constantTime)
-          xData.push(constant)
-          var down = Number((this.listTime.constantTemp - this.listTime.endTemp) / this.listTime.downSpeed) + Number(constant)
-          xData.push(down)
+      var temp = this.listTime.tempControl
+      var xData = [0, temp.startTime]
+      var upTime = Number((temp.constantTemp - temp.startTemp) / temp.upSpeed) + Number(temp.startTime)
+      xData.push(upTime)
+      var constant = upTime + Number(temp.constantTime)
+      xData.push(constant)
+      var down = Number((temp.constantTemp - temp.endTemp) / temp.downSpeed) + Number(constant)
+      xData.push(down)
 
-          var yData = [this.listTime.startTemp, this.listTime.startTemp, this.listTime.constantTemp, this.listTime.constantTemp, this.listTime.endTemp]
+      var yData = [temp.startTemp, temp.startTemp, temp.constantTemp, temp.constantTemp, temp.endTemp]
 
-          this.setOptionData(xData, yData)
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
+      this.setOptionData(xData, yData)
     },
     setOptionData(xData, yData) {
       this.chart.setOption({
